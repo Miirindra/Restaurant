9dd52d6998e35a515ab13666b0860697685948ab
Hery Mirindra <hmirindra@gmail.com>
2019-08-06 09:27:57 +0300

    add method: -displaySelectedSide -displaySelectedDrink -displayAvailableSide -displayAvailableDrink

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -10,12 +10,12 @@ public class Order {
         System.out.println("Choix menu\n1 - Poulet\n2 - Boeuf\n3 - végétarien");
         System.out.println("Que souhaitez-vous comme menu ?");
     }
+
     /**
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-//        System.out.println("Vous avez choisi le menu "+ nbMenu);
         if (nbMenu == 1)
             System.out.println("Vous avez choisi comme menu : poulet");
         else if (nbMenu == 2)
@@ -25,6 +25,7 @@ public class Order {
         else
             System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
     }
+
     /**
      * Run asking process for a menu
      */
@@ -34,4 +35,89 @@ public class Order {
         int nb = sc.nextInt();
         this.displaySelectedMenu(nb);
     }
+
+    /**
+     * Display a selected side depending on all sides enable or not
+     * All sides = vegetables, fries and rice
+     * No all sides = rice or not
+     * @param nbSide The selected side
+     * @param allSideEnable enable display for all side or not
+     */
+    public void displaySelectedSide(int nbSide, boolean allSideEnable) {
+        if (allSideEnable) {
+            switch (nbSide) {
+                case 1:
+                    System.out.println("Vous avez choisi comme accompagnement : légumes frais");
+                    break;
+                case 2:
+                    System.out.println("Vous avez choisi comme accompagnement : frites");
+                    break;
+                case 3:
+                    System.out.println("Vous avez choisi comme accompagnement : riz");
+                    break;
+                default:
+                    System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
+                    break;
+            }
+        }
+        else {
+            switch (nbSide) {
+                case 1:
+                    System.out.println("Vous avez choisi comme accompagnement : riz");
+                    break;
+                case 2:
+                    System.out.println("Vous avez choisi comme accompagnement : pas de riz");
+                    break;
+                default:
+                    System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Display a selcted drink
+     * @param nbDrink The selected drink
+     */
+    public void displaySelectedDrink(int nbDrink) {
+        switch (nbDrink) {
+            case 1:
+                System.out.println("Vous avez choisi comme boisson : eau plate");
+                break;
+            case 2:
+                System.out.println("Vous avez choisi comme boisson : eau gazeuse");
+                break;
+            case 3:
+                System.out.println("Vous avez choisi comme boisson : soda");
+                break;
+            default:
+                System.out.println("Vous n'avez pas choisi de boisson parmi les choix proposés");
+                break;
+        }
+    }
+
+    /**
+     * Display all available sides depending on all sides enable or not.
+     * All sides = vegetables, frites and rice
+     * No all sides = rice or not
+     * @param allSideEnable enable display for all side or not
+     */
+    public void displayAvailableSide(boolean allSideEnable) {
+        System.out.println("Choix accompagnement");
+        if (allSideEnable)
+            System.out.println("1 - légumes frais\n2 - frites\n3 - riz");
+        else
+            System.out.println("1 - riz\n2 - pas de riz");
+        System.out.println("Que souhaitez-vous comme accompagnement ?");
+    }
+
+    /**
+     * Display all available drink in restaurant
+     */
+    public void displayAvailableDrink() {
+        System.out.println("Choix boisson");
+        System.out.println("1 - eau plate\n2 - eau gazeuse\n3 - soda");
+        System.out.println("Que souhaitez-vous comme boisson ?");
+    }
+
 }
